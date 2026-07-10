12ad8b9a848e40793908a06a32cd7d12d271799b
María Cecilia Merino <cecimerino@hotmail.com>
2026-07-10 11:22:00 -0300

    Fix repair script undefined output check

--- scripts/01_preprocessing/repair_reads.sh
+++ scripts/01_preprocessing/repair_reads.sh
@@ -1,33 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Activate the appropriate conda environment if needed
-# conda activate bioinfo
-
-# Repairs paired-end FASTQ files using BBTools Repair.
-
-# Usage:
-# cat sample_list.txt | xargs -n 1 bash repair_reads.sh
+# Repair paired-end reads after preprocessing.
+#
+# Original usage:
+#   cat CleanCumplifyFile.txt | xargs -n 1 bash repair_reads.sh
+#
+# Input:
+#   One R1 FASTQ filename provided as the first argument.
+#   The corresponding R2 filename is inferred by replacing 1.fastq.gz with 2.fastq.gz.
 
 echo
 echo "Processing $1"
-echo 
+echo
 
-# Set the directory containing the FASTQ files
-DIR="/path/to/fastq/files"
+DIR=/media/danilo/Tera2/Analysis/CeciMerino/Analysis_pipeline/00_Fastp
 
 R1=$1
 R2=${R1/1.fastq.gz/2.fastq.gz}
-name=${R1/_1.fastq.gz}
+name=${R1/_1.fastq.gz/}
 
 echo
 echo "R1: $R1"
 echo "R2: $R2"
 echo
 
-if [ -f "./reports/$html.html" ]; then
-	echo "Sample $R1 already processed. Skipping."
-else
-	repair.sh in1=$DIR/$R1 -in2=$DIR/$R2 out1=repair_$R1 -out2=repair_$R2 \
-	outs=single_$name.fq -Xmx48g ziplevel=5
-fi
-
+repair.sh \
+    in1=$DIR/$R1 \
+    in2=$DIR/$R2 \
+    out1=repair_$R1 \
+    out2=repair_$R2 \
+    outs=single_$name.fq \
+    -Xmx48g \
+    ziplevel=5
